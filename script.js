3e29d124b7d2efbb3e681c5ce9852f8fc8035c92
Pearl Jones <jonesatlakota@yahoo.com>
2023-06-28 23:44:35 -0500

    working on prompts

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 var generateBtn = document.querySelector("#generate");
 
-var desiredpwlength = 0;
+var desiredPwLength = 0;
 var passwordarray = "";
 var wantLowerCase = true;
 var wantUpperCase = true;
@@ -12,30 +12,74 @@ var wantSpecialChars =true;
 var minLength = 8;
 var maxLength = 128;
 var inputLength;
+var pwLength = 0;
 
-//is the length within the range for password?
-desiredpwlength=20;
+// this function prompts user to enter the desired length
+// if not between 8 and 128, ask again
+function asklength() {
+pwLength = prompt("Please enter your desired password length, minimum of 8, max of 128");
+  if (pwLength >= 8 && pwLength <=128) {
+    console.log(pwLength);}
+  else {
+    asklength();
+} 
+  return;
+}
 
-//if (inputlength >= minlength) && (inputlength <= maxLength) {
- //continue
- //else ask again
+asklength();
+desiredPwLength = pwLength;
 
+function askLowerCase() {
+  var lowerCaseYN = "";
+  lowerCaseYN = prompt ("Do you want Lower case in your password? Y or N");
+  if (lowerCaseYN == "Y" || lowerCaseYN == "y") {
+    wantLowerCase = true
+  } else { 
+    if (lowerCaseYN == "N" || lowerCaseYN == "n") {
+    wantLowerCase = false;}
+    else {
+      askLowerCase();
+    }
+  }
+return;
+}
 
- tempCharSelection="";
+askLowerCase();
+console.log("wantLowerCase " + wantLowerCase);
+tempCharSelection="";
 
  // Do we want lowercase letters?
  // Then add the lowercase letters to our array of choice of characters for our password
- if (wantLowerCase) {
-  charSelection = "abcdefghijklmnopqrstuvwxyz";
-  tempCharSelection = tempCharSelection.concat(charSelection);
- }
+if (wantLowerCase) {
+  charSelection = "abcdefghijklmnopqrstuvwxyz"; }
+else {
+  charSelection = "";}
+tempCharSelection = tempCharSelection.concat(charSelection);
 
 console.log("after wantLower " + tempCharSelection);
 
 // Do we want uppercase letters?  
 // Then add the uppercase letters to our array of choice of characters for our password
+function askUpperCase() {
+  var upperCaseYN = "";
+  upperCaseYN = prompt ("Do you want Upper case in your password? Y or N");
+  if (upperCaseYN == "Y" || upperCaseYN == "y") {
+    wantUpperCase = true
+  } else { 
+    if (upperCaseYN == "N" || upperCaseYN == "n") {
+    wantUpperCase = false;}
+    else {
+      askUpperCase();
+    }
+  }
+return;
+}
+
+askUpperCase();
+console.log("wantUpperCase " + wantUpperCase);
+var alphabet="abcdefghijklmnopqrstuvwxyz";
 if (wantUpperCase) {
-  charSelection = charSelection.toUpperCase();
+  charSelection = charSelection.toUpperCase(alphabet);
   tempCharSelection = tempCharSelection.concat(charSelection);
 }
 console.log("after WantUpper " + tempCharSelection);
@@ -55,7 +99,7 @@ if (wantSpecialChars) {
   tempCharSelection = tempCharSelection.concat(charSelection);
 }
 console.log("after Special Chars  " + tempCharSelection);
-var arraylength = tempCharSelection.length;
+var arraylength = tempCharSelection.length - 1;
 console.log("charselection length " + arraylength);
 
 var tempInteger = 0;
@@ -74,7 +118,7 @@ var passwordText = "";
 // to the Random Integer, the found character equivalend to the cell is our character password concatenated
 // to the string of our new password
 
-for (var i=0; i<desiredpwlength; i++) {
+for (var i=0; i < desiredPwLength; i++) {
   getRandomInt();
   temppassword = tempCharSelection[tempInteger];
   console.log("current pw " + temppassword);
