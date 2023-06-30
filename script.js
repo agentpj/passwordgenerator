a5f44029ae7b1615df85afb567127ee7beabc76e
Pearl Jones <jonesatlakota@yahoo.com>
2023-06-30 17:42:40 -0500

    fixed the issue with the password length looping

--- script.js
+++ script.js
@@ -13,51 +13,51 @@ var charsNumeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 // This is the function that asks the user several prompts to use to generate the password
 // depending on password length, and type of characters
 function generatePassword() {
-  //var passwordText = document.querySelector("#password");
-  //passwordText.value = "";
   var pwLength = prompt("Please enter your desired password length, minimum of 8, max of 128");
+  // did user specify outside the required password length?  If not, try again
   if (pwLength < 8 || pwLength > 128) {
     alert("Please re-enter the password length");
-    generatePassword();
   }
   else {
     var userlowerChoice = prompt("Do you want Lower case in your password? Y or N");
     userlowerChoice = userlowerChoice.toUpperCase();
     var tempCharSelection = [];
-
+    // if user wants lower case letters, let's add it to tempCharSelection array
     if (userlowerChoice == "Y") {
       tempCharSelection = tempCharSelection.concat(charsLowerCase);
-      console.log(tempCharSelection);
     }
 
     var userUpperChoice = prompt("Do you want Upper case in your password? Y or N");
     userUpperChoice = userUpperChoice.toUpperCase();
+    // if user wants upper case letters, let's add it to the tempCharSelection array
     if (userUpperChoice == "Y") {
       tempCharSelection = tempCharSelection.concat(charsUpperCase);
-      console.log(tempCharSelection);
     }
 
     var userSpecialChoice = prompt("Do you want Special Characters in your password? Y or N");
     userSpecialChoice = userSpecialChoice.toUpperCase();
+    // if user wants special characters, let's add it to the tempCharSelection array
     if (userSpecialChoice == "Y") {
       tempCharSelection = tempCharSelection.concat(charSpecial);
-      console.log(tempCharSelection);
     }
 
     var userNumbersChoice = prompt("Do you want numbers in your password? Y or N");
     userNumbersChoice = userNumbersChoice.toUpperCase();
+    // if user wants integers or numbers, let's add it to the tempCharSelection array
     if (userNumbersChoice == "Y") {
       tempCharSelection = tempCharSelection.concat(charsNumeric);
-      console.log(tempCharSelection);
     }
 
     var password = "";
-
+    // let's find out if user selected any character choices for the password by length of the array
     if (tempCharSelection.length < 1) {
       alert("You need to make at least one selection");
       return;
     }
 
+    // this for loop creates a random integer and we will find the equivalent to a cell # in the tempCharSelection array and the
+    // equivalent character of the cell # will be the character in the user password.  The equivalent character gets put into the password
+    // variable until the desired password length is reached
     for (var i = 0; i < pwLength; i++) {
       var firstLetter = tempCharSelection[(Math.floor(Math.random() * tempCharSelection.length))];
       password = password.concat(firstLetter);
@@ -71,18 +71,14 @@ function generatePassword() {
 // Write password to the #password input
 
 function writePassword() {
-
   var passwordText = document.querySelector("#password");
   passwordText.value = "";
+  // need to clear out the screen before regenerating a new password
   setTimeout(() => {
     var password = generatePassword();
     passwordText.value = password;
-  }, 1000);
-
+  }, 500);
 }
-// call the function that generates the password and returns it so we can display
-//writePassword();
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
